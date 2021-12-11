a687c8a78d4a856e1819718816d8a12b162d9c4c
Moffein <22742925+Moffein@users.noreply.github.com>
2021-12-10 21:29:23 -0700

    Tweaked Origin drop odds.

--- Risky_Artifacts/Artifacts/Origin.cs
+++ Risky_Artifacts/Artifacts/Origin.cs
@@ -326,8 +326,8 @@ namespace Risky_Artifacts.Artifacts
 
             float whiteChance = (tier == EliteTier.None || (tier == EliteTier.T1 && CombatDirector.IsEliteOnlyArtifactActive() && !Origin.ignoreHonor)) ? 50f : 0f;
             float greenChance = tier < EliteTier.T2 ? 35f : 0f;
-            float redChance = 5f;
-            float yellowChance = tier < EliteTier.T2 ? 10f : 0.55f;
+            float redChance = tier < EliteTier.T2 ? 5f : 30f;
+            float yellowChance = 10f;
 
             float total = whiteChance + greenChance + redChance + yellowChance;
             
