e5e132b075a90fea4fe7b183b1637c027958f923
Moffein <22742925+Moffein@users.noreply.github.com>
2022-11-25 15:19:14 -0800

    Fixed PrimordialTele.cs not being uploaded to the repo.

---
+++ Risky_Artifacts/Artifacts/PrimordialTele.cs
@@ -0,0 +1,100 @@
+﻿
+using UnityEngine;
+using RoR2;
+using R2API;
+using System.Collections.Generic;
+using MonoMod.Cil;
+using System;
+using UnityEngine.Networking;
+
+namespace Risky_Artifacts.Artifacts
+{
+    public class PrimordialTele
+    {
+        public static bool enabled = true;
+        public static bool enableOnFirstLoop = false;
+        public static bool forceEnable = false;
+        public static ArtifactDef artifact;
+        private static SpawnCard teleCard;
+        private static SpawnCard primordialTeleCard;
+
+        private static bool first = true;
+
+        public PrimordialTele()
+        {
+            if (!enabled && !forceEnable) return;
+
+            if (enabled)
+            {
+                artifact = ScriptableObject.CreateInstance<ArtifactDef>();
+                artifact.cachedName = "RiskyArtifactOfPrimacy";
+                artifact.nameToken = "RISKYARTIFACTS_PRIMORDIAL_NAME";
+                artifact.descriptionToken = "RISKYARTIFACTS_PRIMORDIAL_DESC";
+                artifact.smallIconDeselectedSprite = RiskyArtifactsPlugin.assetBundle.LoadAsset<Sprite>("texPrimacyDisabled.png");
+                artifact.smallIconSelectedSprite = RiskyArtifactsPlugin.assetBundle.LoadAsset<Sprite>("texPrimacyEnabled.png");
+                RiskyArtifactsPlugin.FixScriptableObjectName(artifact);
+                ContentAddition.AddArtifactDef(artifact);
+            }
+
+            teleCard = LegacyResourcesAPI.Load<SpawnCard>("spawncards/interactablespawncard/iscTeleporter");
+            primordialTeleCard = LegacyResourcesAPI.Load<SpawnCard>("spawncards/interactablespawncard/iscLunarTeleporter");
+
+            LegacyResourcesAPI.Load<GameObject>("prefabs/networkedobjects/teleporters/LunarTeleporterProngs").AddComponent<PrimordialTeleInitialState>();
+
+            IL.RoR2.SceneDirector.PlaceTeleporter += (il) =>
+            {
+                ILCursor c = new ILCursor(il);
+                c.GotoNext(
+                     x => x.MatchLdfld<SceneDirector>("teleporterSpawnCard")
+                    );
+                c.GotoNext(
+                     x => x.MatchLdfld<SceneDirector>("teleporterSpawnCard")
+                    );
+                c.Index++;
+                c.EmitDelegate<Func<SpawnCard, SpawnCard>>(origTeleporter =>
+                {
+                    if ((Run.instance.stageClearCount >= 5 || enableOnFirstLoop) && (forceEnable || (enabled && RunArtifactManager.instance.IsArtifactEnabled(artifact.artifactIndex))) && origTeleporter == teleCard)
+                    {
+                        origTeleporter = primordialTeleCard;
+                    }
+                    return origTeleporter;
+                });
+            };
+
+            On.RoR2.Stage.Start += (orig, self) =>
+            {
+                first = true;
+                orig(self);
+            };
+
+            On.EntityStates.LunarTeleporter.IdleToActive.OnExit += (orig, self) =>
+            {
+                if (!first || ((Run.instance.stageClearCount + 1) % 5 == 0))
+                {
+                    first = false;
+                    orig(self);
+                }
+                else
+                {
+                    first = false;
+                }
+            };
+        }
+    }
+
+    public class PrimordialTeleInitialState : MonoBehaviour
+    {
+        public void Start()
+        {
+            if (NetworkServer.active)
+            {
+                if ((Run.instance.stageClearCount + 1) % 5 != 0)
+                {
+                    EntityStateMachine esm = base.GetComponent<EntityStateMachine>();
+                    esm.SetNextState(new EntityStates.LunarTeleporter.Idle());
+                }
+            }
+            Destroy(this);
+        }
+    }
+}
